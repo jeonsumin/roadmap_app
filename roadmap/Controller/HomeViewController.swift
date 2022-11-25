cc72b09d17e17286521e60dde1d91e5d4bc2107e
jeonsumin <soomis7@naver.com>
2022-11-25 14:26:51 +0900

    init

---
+++ roadmap/Controller/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  roadmap
+//
+//  Created by deepvisions on 2022/11/25.
+//
+
+import UIKit
+
+class HomeViewController:UIViewController {
+    
+    
+    //MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUIComponents()
+    }
+    
+    //MARK: - Helper
+    func configureUIComponents(){
+        view.backgroundColor = .systemBackground
+    }
+}
+
